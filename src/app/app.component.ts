5024c350706f83944e179cea3e22957daf4670d5
andreaskst <andreas.kavlitsis@gmail.com>
2019-10-10 12:15:58 +0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from "@angular/core";
+import { Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent {
+  name = "Angular";
+  counter: any;
+  count: number;
+  private subscription: Subscription;
+
+  startObservable(): any {
+          this.counter = new Observable<number>(observer => {
+            console.log('Subscribed');
+
+            let index = -1;
+            const interval = setInterval(() => {
+              index++;
+              console.log(`next: ${index}`);
+              observer.next(index);
+            }, 1000);
+
+            // teardown
+            return () => {
+              console.log('Teardown');
+              clearInterval(interval);
+            }
+    
+    }
+    return this.counter;
+  }
+
+  callObservable() {
+    this.subscription = this.counter.subscribe(
+    (value) => this.count = value,
+    (error) => console.error(error),
+    () => console.log('complete'));
+  }
 }
